834c2456a3a9aeb9aa5bc77b8349a0143cb19a67
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-06 16:18:17 -0500

    Writing the Player script.

--- 2D Roguelike/Assets/Scripts/GameManager.cs
+++ 2D Roguelike/Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
 
     private int level = 3; // Level where enemies appear (for testing).
 
@@ -25,5 +28,10 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 }
